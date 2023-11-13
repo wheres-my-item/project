beb79afdcd1301b2670f9ba458d6c4e969f9d986
daomcgill <77309217+daomcgill@users.noreply.github.com>
2023-11-13 13:00:02 -1000

    Added basic item cards with test data
    To Do:
    - Claim Button
    - Format Cards
    - Page Title
    - Sort/ Filter

---
+++ app/imports/ui/components/FoundItem.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image } from 'react-bootstrap';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const FoundItem = ({ item }) => (
+  <Card className="h-100 mb-2">
+    <Card.Header>
+      <Image src={item.image} width="300px" />
+      <Card.Title>{item.name}</Card.Title>
+      <Card.Subtitle>{item.category}</Card.Subtitle>
+      <Card.Subtitle>{item.color}</Card.Subtitle>
+      <Card.Subtitle>{item.datePosted}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{item.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+FoundItem.propTypes = {
+  item: PropTypes.shape({
+    name: PropTypes.string,
+    category: PropTypes.string,
+    color: PropTypes.string,
+    datePosted: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    // _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default FoundItem;
