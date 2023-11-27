2c5e1d4b147a79def85b03c1951af252397d9233
daomcgill <77309217+daomcgill@users.noreply.github.com>
2023-11-27 13:40:57 -0700

    Added Filter by Category functionality

--- app/imports/ui/pages/FoundItems.jsx
+++ app/imports/ui/pages/FoundItems.jsx
@@ -1,22 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Dropdown, Form } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import FoundItem from '../components/FoundItem';
 import { Items } from '../../api/items/Items';
 
-/* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const FoundItems = () => {
-  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const categories = ['Clothing', 'Electronics', 'Personal Items', 'Bags and Backpacks', 'Books and Notebooks', 'IDs and Cards', 'Miscellaneous'];
+
   const { ready, items } = useTracker(() => {
-    // Note that this subscription will get cleaned up
-    // when your component is unmounted or deps change.
-    // Get access to Stuff documents.
     const subscription = Meteor.subscribe(Items.userPublicationName);
-    // Determine if the subscription is ready
     const rdy = subscription.ready();
-    // Get the Item documents
     const foundItems = Items.collection.find({}).fetch();
     return {
       items: foundItems,
@@ -24,20 +20,52 @@ const FoundItems = () => {
     };
   }, []);
 
-  return (ready ? (
+  // Handle category selection
+  const handleCategorySelection = (e) => {
+    const category = e.target.name;
+    if (e.target.checked) {
+      setSelectedCategories([...selectedCategories, category]);
+    } else {
+      setSelectedCategories(selectedCategories.filter(cat => cat !== category));
+    }
+  };
+
+  // Filter items based on selected categories
+  const filteredItems = selectedCategories.length > 0 ? items.filter(item => selectedCategories.includes(item.category)) : items;
+
+  return ready ? (
     <Container id="found-items-page" className="py-3">
       <Row className="justify-content-center">
-        <Col>
-          <Col className="text-center">
-            <h2>Found Items</h2>
-          </Col>
-          <Row sm={1} md={2} lg={3} className="g-4">
-            {items.map((item) => <Col key={item._id}><FoundItem item={item} /></Col>)}
-          </Row>
+        <Col className="text-center">
+          <h2>Found Items</h2>
         </Col>
       </Row>
+      <Row className="justify-content-end mb-3">
+        <Col md="auto">
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Filter
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {categories.map(category => (
+                <Form.Check
+                  type="checkbox"
+                  id={`check-${category}`}
+                  label={category}
+                  name={category}
+                  onChange={handleCategorySelection}
+                  key={category}
+                />
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
+      <Row sm={1} md={2} lg={3} className="g-4">
+        {filteredItems.map((item) => <Col key={item._id}><FoundItem item={item} /></Col>)}
+      </Row>
     </Container>
-  ) : <LoadingSpinner />);
+  ) : <LoadingSpinner />;
 };
 
 export default FoundItems;
