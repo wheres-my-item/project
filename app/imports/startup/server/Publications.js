c242ba3f947acf8d1908969bf6467fe4d81745a2
daomcgill <77309217+daomcgill@users.noreply.github.com>
2023-11-27 11:43:54 -0700

    Update Publications.js

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-// import { Roles } from 'meteor/alanning:roles';
-// import { Stuffs } from '../../api/stuff/Stuff';
+import { Roles } from 'meteor/alanning:roles';
+import { Stuffs } from '../../api/stuff/Stuff';
 import { Items } from '../../api/items/Items';
 
 // User-level publication.
@@ -17,22 +17,22 @@ Meteor.publish(Items.adminPublicationName, function () {
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise, publish nothing.
-// Meteor.publish(Stuffs.userPublicationName, function () {
-//   if (this.userId) {
-//     const username = Meteor.users.findOne(this.userId).username;
-//     return Stuffs.collection.find({ owner: username });
-//   }
-//   return this.ready();
-// });
+Meteor.publish(Stuffs.userPublicationName, function () {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Stuffs.collection.find({ owner: username });
+  }
+  return this.ready();
+});
 
 // Admin-level publication.
 // If logged in and with admin role, then publish all documents from all users. Otherwise, publish nothing.
-// Meteor.publish(Stuffs.adminPublicationName, function () {
-//   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-//     return Stuffs.collection.find();
-//   }
-//   return this.ready();
-// });
+Meteor.publish(Stuffs.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Stuffs.collection.find();
+  }
+  return this.ready();
+});
 
 // alanning:roles publication
 // Recommended code to publish roles for each user.
