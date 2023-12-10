904de42af3d563d4c5d3dfd25995f4c36c19638d
daomcgill <77309217+daomcgill@users.noreply.github.com>
2023-12-10 12:39:57 -1000

    default items with claim ids

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Items } from '../../api/items/Items';
-// import { Claims } from '../../api/claims/Claims';
+import { Claims } from '../../api/claims/Claims';
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
@@ -23,16 +23,36 @@ const addItems = (item) => {
   return Items.collection.insert(item); // Return the inserted item ID
 };
 
-// Array to store item IDs
-let itemIds = [];
+// // Array to store item IDs
+// let itemIds = [];
 
 if (Items.collection.find().count() === 0) {
   if (Meteor.settings.defaultItems) {
     console.log('Creating default items.');
     // eslint-disable-next-line no-unused-vars
-    itemIds = Meteor.settings.defaultItems.map(item => addItems(item));
+    // itemIds = Meteor.settings.defaultItems.map(item => addItems(item));
+    Meteor.settings.defaultItems.forEach(item => addItems(item));
   }
 }
+
+const addClaim = (claim) => {
+  console.log(`Adding claim for item ID: ${claim.itemId}`);
+  Claims.collection.insert(claim);
+};
+
+Meteor.setTimeout(() => {
+  if (Claims.collection.find().count() === 0 && Meteor.settings.defaultClaims) {
+    console.log('Creating default claims.');
+
+    const itemIds = Items.collection.find({}, { limit: 5 }).map(item => item._id);
+    Meteor.settings.defaultClaims.forEach((claim, index) => {
+      if (itemIds[index]) {
+        const claimWithItemId = { ...claim, itemId: itemIds[index] };
+        addClaim(claimWithItemId);
+      }
+    });
+  }
+}, 500);
 
 const createTTLIndex = () => {
   const collection = Items.collection.rawCollection();
