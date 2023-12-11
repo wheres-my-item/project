70db02c702caaa371a88d767839b9196c6eb47fb
mnakagawa14 <134327026+mnakagawa14@users.noreply.github.com>
2023-12-10 17:01:04 -1000

    Removed Stuffs

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,22 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Items } from '../../api/items/Items';
 import { Claims } from '../../api/claims/Claims';
 /* eslint-disable no-console */
-
-// Initialize the database with a default data document.
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-};
-
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
 
 const addItems = (item) => {
   console.log(`  Adding: ${item.name} (${item.category})`);
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -32,25 +32,6 @@ Meteor.publish(Claims.adminPublicationName, function publish(itemId) {
   return this.ready();
 });
 
-// User-level publication.
-// If logged in, then publish documents owned by this user. Otherwise, publish nothing.
-Meteor.publish(Stuffs.userPublicationName, function () {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.collection.find({ owner: username });
-  }
-  return this.ready();
-});
-
-// Admin-level publication.
-// If logged in and with admin role, then publish all documents from all users. Otherwise, publish nothing.
-Meteor.publish(Stuffs.adminPublicationName, function () {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.collection.find();
-  }
-  return this.ready();
-});
-
 // alanning:roles publication
 // Recommended code to publish roles for each user.
 Meteor.publish(null, function () {
